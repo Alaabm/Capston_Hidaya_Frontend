1a6daa00f17c6834820ea3324289230b86928b0c
Alaabm <alaabasim@gmail.com>
2024-02-16 17:06:15 -0500

    completing content for morning athkar

--- src/pages/EveningAthkar.jsx
+++ src/pages/EveningAthkar.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import InfoCollapse from "../components/InfoCollapse";
 
 const EveningAthkar = () => {
   return (
-    <div>EveningAthkar</div>
+    <>
+    <div>
+    <h1>Evening Remembrance</h1>
+    <div className="ayat-alkursi">
+        <p style={{ fontSize: "17pt" }}>
+          
+        </p>
+        <div className="evening-collapse">
+          <InfoCollapse buttonText="Transliteration">
+            <p>
+              
+            </p>
+          </InfoCollapse>
+          <br />
+          <InfoCollapse buttonText="Translation">
+            <p>
+              
+            </p>
+          </InfoCollapse>
+        </div>
+      </div>
+<hr />
+
+
+
+    </div>
+    </>
+    
   )
 }
 
