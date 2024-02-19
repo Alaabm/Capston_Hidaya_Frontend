bdb870f9efff7b4cfd5aedca3165f7bf41c9a3ab
Alaabm <alaabasim@gmail.com>
2024-02-19 04:01:49 -0500

    in the process of dua card-body

--- src/pages/Duas.jsx
+++ src/pages/Duas.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ButtonLink from "../components/ButtonLink";
-// import "../styles/Dua.css";
+import "../styles/Dua.css";
 // import "../App.jsx";
 
 const Duas = () => {
   return (
     <>
-      <div className="dua-container">
+    <div className="dua-container">
         <div>
           <ButtonLink
             route="./morningAthkar"
@@ -37,25 +37,66 @@ const Duas = () => {
             buttonLinkText="Istikhara:The Prayer of Seeking Guidance/"
           ></ButtonLink>
         </div>
-          <div>
+        <div>
           <ButtonLink
             route="./calamityDua"
             buttonLinkText="When Afflicted by Calamity"
           ></ButtonLink>
-          </div>
-          <div>
+        </div>
+        <div>
           <ButtonLink
             route="./successDua"
             buttonLinkText="Dua for Success"
           ></ButtonLink>
-          </div>
-          <div>
+        </div>
+        <div>
           <ButtonLink
             route="./anxietyDua"
             buttonLinkText="Anxiety and Worry"
           ></ButtonLink>
+        </div>
+
+
+    <div className="card-row">
+
+          <div className="card">
+            <div className="card-body">
+              <h5 style={{className:"card-title"}}>Anxiety and Worry</h5>
+                <ButtonLink
+              route="./anxietyDua"
+              buttonLinkText="Anxiety and Worry"
+            ></ButtonLink>
+            </div>
           </div>
+            
+          <div className="row">
+            <div className="card-body">
+              <h5 style={{className:"card-title"}}>Dua for Success</h5>
+              <ButtonLink
+            route="./successDua"
+            buttonLinkText="Dua for Success"
+          ></ButtonLink>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="column">
+
+                  <div className="card-body">
+                      <h5 style={{className:"card-title"}}>When Afflicted by Calamity</h5>
+                      <ButtonLink
+                    route="./calamityDua"
+                    buttonLinkText="When Afflicted by Calamity"
+                  ></ButtonLink>
+                    </div>
+
+            </div>
+            
+          </div>
+          
+          
       </div>
+    </div>
     </>
   );
 };
